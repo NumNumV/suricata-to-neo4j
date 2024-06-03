9d528299d1d04ed464f93a5516aaff3b586e3824
NumNum <75941535+NumNumV@users.noreply.github.com>
2024-06-03 19:44:10 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+# Elasticsearch
+ELASTICSEARCH_HOST = 'http://IP:9200'
+ELASTICSEARCH_USER = 'elastic'
+ELASTICSEARCH_PASSWORD = 'password'
+# Neo4j
+NEO4J_HOST = 'bolt://IP:7687'
+NEO4J_USER = 'neo4j'
+NEO4J_PASSWORD = 'password'
+NEO4J_DATABASE = 'neo4j'
